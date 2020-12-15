54559ae1633347474f11f523b56a8702b0bca7fd
“teji1998” <tejasvinirpk@gmail.com>
2020-12-15 18:59:20 +0530

    Added:Query to create a table employee_payroll

--- empPayrollQueries.sql
+++ empPayrollQueries.sql
@@ -3,3 +3,11 @@
 create database Employee_Payroll_Service;
 --To use a database
 use Employee_Payroll_Service;
+
+/*UC2*/
+--Creating a table
+CREATE TABLE Employee_Payroll (
+    ID int identity(1,1) primary key,
+    Name varchar(50),
+    Salary Float,
+    Start_Date varchar(20));
