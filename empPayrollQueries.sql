976fbb81ec8aa12a5ec6a0d8c2bdc9d1a45810d4
“teji1998” <tejasvinirpk@gmail.com>
2020-12-15 19:24:37 +0530

    Added:Query to insert values into the employee payroll table

--- empPayrollQueries.sql
+++ empPayrollQueries.sql
@@ -6,8 +6,20 @@ use Employee_Payroll_Service;
 
 /*UC2*/
 --Creating a table
-CREATE TABLE Employee_Payroll (
-    ID int identity(1,1) primary key,
-    Name varchar(50),
-    Salary Float,
-    Start_Date varchar(20));
+CREATE TABLE Employee_Payroll (ID int identity(1,1) primary key,Name varchar(50),Salary Float,
+Start_Date varchar(20));
+
+/*UC3*/
+--Inserting values into table
+INSERT INTO Employee_Payroll(Name, Salary,Start_Date)
+VALUES ('Sravani', '50000', '2020-12-14');
+INSERT INTO Employee_Payroll(Name, Salary,Start_Date)
+VALUES ('Bhagya', '60000', '2020-12-15');
+INSERT INTO Employee_Payroll(Name, Salary,Start_Date)
+VALUES ('Teju', '40000', '2020-04-14');
+INSERT INTO Employee_Payroll(Name, Salary,Start_Date)
+VALUES ('Aditya', '700000', '2020-08-23');
+INSERT INTO Employee_Payroll(Name, Salary,Start_Date)
+VALUES ('Zeesh', '800000', '2020-02-29');
+
+	
